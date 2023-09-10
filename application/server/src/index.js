b78852b52766f8e48fcf12ad7dbe565e95093739
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-09-10 13:16:36 +0300

    cors package added

--- application/server/src/index.js
+++ application/server/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 
 const v1_OrderRouter = require('./v1/routes/OrderRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
     res.send('TEST TEST');
