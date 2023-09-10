c3de424ec556c1f4b66dec020652a79f4e77f202
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-09-10 07:15:08 +0300

    controller added for Blockchain API

---
+++ application/server/src/controllers/orderController.js
@@ -0,0 +1,106 @@
+const orderService = require("../services/orderService");
+
+const getAllOrders = async (req, res) => {
+    try {
+        const allOrders = await orderService.getAllOrders();
+        res.send({ status: "OK", data: allOrders });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+};
+
+const getOneOrder = async (req, res) => {
+    // TODO: orderId params checking
+    const {
+        params: { orderId }
+    } = req;
+    if (!orderId) {
+        res
+            .status(400)
+            .send({
+                status: "FAILED",
+                data: { error: "Parameter ':orderId' can not be empty" }
+            });
+    }
+    try {
+        const order = await orderService.getOneOrder(orderId);
+        res.send({ status: "OK", data: order });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+};
+
+const createNewOrder = async (req, res) => {
+    const { body } = req;
+    if (
+        !body.order ||
+        !body.status ||
+        !body.createdAt
+    ) {
+        res
+            .status(400)
+            .send({
+                status: "FAILED",
+                data: {
+                    error:
+                        "One of the following key is missing or is empty in request body"
+                }
+            });
+    }
+
+    const newOrder = {
+        orderId: body.orderId,
+        assetId: body.assetId,
+        assetName: body.assetName,
+        price: body.price,
+        quantity: body.quantity,
+        senderId: body.senderId,
+        receiveId: body.receiveId,
+        status: body.status,
+        trackingInfo: [],
+        createdAt: body.createdAt,
+        updatedAt: body.updatedAt
+    };
+    try {
+        const createdOrder = await orderService.createNewOrder(newOrder);
+        res.status(201).send({ status: "OK", data: createdOrder });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+};
+
+const updateOneOrder = (req, res) => {
+    const {
+        body,
+        params: { orderId }
+    } = req;
+    if (!orderId) {
+        res
+            .status(400)
+            .send({
+                status: "FAILED",
+                data: { error: "Parameter ':orderId' can not be empty" }
+            });
+    }
+    try {
+        const updatedOrder = orderService.updateOneOrder(orderId, body);
+        res.send({ status: "OK", data: updatedOrder });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+};
+
+module.exports = {
+    getAllOrders,
+    getOneOrder,
+    createNewOrder,
+    updateOneOrder
+};
--- application/server/src/v1/routes/orderRoutes.js
+++ application/server/src/v1/routes/orderRoutes.js
@@ -1,6 +1,14 @@
 const express = require('express');
-// const orderController = require('../../controllers/orderController');
+const orderController = require('../../controllers/orderController');
 
 const router = express.Router();
+
+router.get("/", orderController.getAllOrders);
+
+router.get("/:orderId", orderController.getOneOrder);
+
+router.post("/", orderController.createNewOrder);
+
+router.patch("/:orderId", orderController.updateOneOrder);
 
 module.exports = router;
